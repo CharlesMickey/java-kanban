3c7d718cf1f81aed5fbf0c8c48e4bbb9f9421055
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-07-13 22:09:31 +0300

    Feat: добавить восстановление истории из файла

--- src/taskFileManager/FileBackedTasksManager.java
+++ src/taskFileManager/FileBackedTasksManager.java
@@ -23,10 +23,10 @@ public class FileBackedTasksManager
 
   public FileBackedTasksManager(String defaultFilePath) {
     this.filePath = Paths.get(defaultFilePath);
-    loadFromFile(defaultFilePath);
   }
 
-  private void loadFromFile(String path) {
+  static FileBackedTasksManager loadFromFile(String path) {
+    FileBackedTasksManager fileBackedTasksManager = new FileBackedTasksManager(path);
     try (
       BufferedReader fileData = Files.newBufferedReader(
         Path.of(path),
@@ -43,31 +43,38 @@ public class FileBackedTasksManager
         Task task = fromStringTask(line);
         if (task instanceof Epic) {
           Epic epic = (Epic) task;
-          super.setEpic(epic.getId(), epic);
+          fileBackedTasksManager.setEpic(epic.getId(), epic);
         } else if (task instanceof Subtask) {
           Subtask subtask = (Subtask) task;
-          super.setSubtask(subtask.getId(), subtask);
+          fileBackedTasksManager.setSubtask(subtask.getId(), subtask);
         } else {
-          super.setTask(task.getId(), task);
+          fileBackedTasksManager.setTask(task.getId(), task);
         }
         line = fileData.readLine();
       }
 
-      for (int taskId : historyFromString(fileData.readLine())) {
-        Task task = getTaskOfAnyTypeById(taskId);
+      line = fileData.readLine();
+      if(line == null) {
+        return fileBackedTasksManager;
+      }
+
+      for (int taskId : historyFromString(line)) {
+        Task task = fileBackedTasksManager.getTaskOfAnyTypeById(taskId);
         if (task instanceof Epic) {
-          getEpic(taskId);
+          fileBackedTasksManager.getEpic(taskId);
         } else if (task instanceof Subtask) {
-          getSubtask(taskId);
+          fileBackedTasksManager.getSubtask(taskId);
         } else {
-          getTask(taskId);
+          fileBackedTasksManager.getTask(taskId);
         }
       }
     } catch (IOException exception) {
-      throw new ManagerSaveException(
+      System.out.println(
         "Произошла ошибка при получении данных в файл."
       );
     }
+
+    return fileBackedTasksManager;
   }
 
   private void save() {
@@ -252,12 +259,80 @@ public class FileBackedTasksManager
       "./resources/data.csv"
     );
 
+    Task task = new Task(
+            Type.TASK,
+            "В магаз",
+            "Купить 5 шоколадок",
+            Status.NEW
+    );
+    Task task2 = new Task(
+            Type.TASK,
+            "В Макдак",
+            "Купить пару флурри и роял",
+            Status.NEW
+    );
+
+    Epic epic1 = new Epic(
+            Type.EPIC,
+            "Подготовка к путешествию",
+            "Подготовиться к поездке на озеро"
+    );
+    Epic epic2 = new Epic(
+            Type.EPIC,
+            "Ремонт в квартире",
+            "Сделать ремонт в падике"
+    );
+
+    Subtask subtask1 = new Subtask(
+            Type.SUBTASK,
+            "Купить билеты",
+            "Купить билеты на поезд в Анапу",
+            Status.NEW,
+            epic1.getId()
+    );
+
+    Subtask subtask2 = new Subtask(
+            Type.SUBTASK,
+            "Забронировать отель",
+            "Забронировать номер в отеле в СПб",
+            Status.NEW,
+            epic1.getId()
+    );
+
+    Subtask subtask3 = new Subtask(
+            Type.SUBTASK,
+            "Купить краску",
+            "Купить краску – фиолетовую",
+            Status.NEW,
+            epic2.getId()
+    );
+
+    fileBackedTasksManager.setTask(task.getId(), task);
+    fileBackedTasksManager.setTask(task2.getId(), task2);
+
+    fileBackedTasksManager.setEpic(epic1.getId(), epic1);
+    fileBackedTasksManager.setEpic(epic2.getId(), epic2);
+
+    fileBackedTasksManager.setSubtask(subtask1.getId(), subtask1);
+    fileBackedTasksManager.setSubtask(subtask2.getId(), subtask2);
+    fileBackedTasksManager.setSubtask(subtask3.getId(), subtask3);
+
+    fileBackedTasksManager.getTask(1);
+    fileBackedTasksManager.getTask(2);
+    fileBackedTasksManager.getEpic(3);
+    fileBackedTasksManager.getEpic(4);
+    fileBackedTasksManager.getSubtask(5);
+    fileBackedTasksManager.getSubtask(6);
+    fileBackedTasksManager.getSubtask(7);
+
+    FileBackedTasksManager fbtm =  fileBackedTasksManager.loadFromFile("./resources/data.csv");
+
     System.out.println("\nВесь список восстановленных задач");
-    System.out.println(fileBackedTasksManager.getAllEpics());
-    System.out.println(fileBackedTasksManager.getAllTasks());
-    System.out.println(fileBackedTasksManager.getAllSubtasks());
+    System.out.println(fbtm.getAllEpics());
+    System.out.println(fbtm.getAllTasks());
+    System.out.println(fbtm.getAllSubtasks());
 
     System.out.println("\nВосстановленная история");
-    System.out.println(fileBackedTasksManager.getHistory());
+    System.out.println(fbtm.getHistory());
   }
 }
