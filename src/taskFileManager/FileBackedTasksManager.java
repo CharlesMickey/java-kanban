f31eff9d24c4ff7efae6c3d3fd8e1e6480e1c117
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-07-15 23:57:38 +0300

    Style: убрать лишнии комментарии

--- src/taskFileManager/FileBackedTasksManager.java
+++ src/taskFileManager/FileBackedTasksManager.java
@@ -93,7 +93,6 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     }
   }
 
-  // С двойным append не сообразил что так можно)
   static String historyToString(List<Task> tasks) {
     StringBuilder history = new StringBuilder("");
 
