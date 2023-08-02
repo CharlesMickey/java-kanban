726ea4fafed426a49af12033530e001b24e2c0ca
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2023-08-02 22:18:13 +0300

    Ref: удалить current exception метод без аргументов

--- src/currentThrows/ManagerSaveException.java
+++ src/currentThrows/ManagerSaveException.java
@@ -2,8 +2,6 @@ package currentThrows;
 
 public class ManagerSaveException extends RuntimeException {
 
-  public ManagerSaveException() {}
-
   public ManagerSaveException(String message) {
     super(message);
   }
